6a47b835f6936e544aebaf3de531602f97e71f27
speto61 <speto61@gmail.com>
2024-09-27 17:17:12 -0400

    Remote commit

--- Data & Vars Types/bread.cpp
+++ Data & Vars Types/bread.cpp
@@ -1,3 +1,5 @@
+//Tyler Ramey, CSC 114, 9/27/2024, Module 3: Vars and Data Types
+
 #include <iostream> 
 using namespace std; 
 
@@ -17,11 +19,13 @@ int main() {
 	cout << "Enter number of grams of water needed for one load of bread\n";
 	cin >> water; 
 	cout << "How many loaves of bread would you like to make?\n"; 
+	
 	cin >> bread; 
 	flour = flour * bread;
 	salt = salt * bread;
 	yeast = yeast * bread;
 	water = water * bread; 
+	
 	cout << "To make " << bread << " loaves of bread you will need " << flour << " grams of flour, " << salt << " grams of salt, " << yeast << " grams of yeast and " << water << " grams of water."; 
 
 }
